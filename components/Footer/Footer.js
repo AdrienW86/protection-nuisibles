f0f3d76aff086e2b5f8585e58acd333adf2d856e
AdrienW86 <adrien_weiss@outlook.fr>
2025-04-26 16:26:22 +0200

    mise en ligne

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -12,8 +12,8 @@ export default function Footer() {
         <Image
             src="/logo.png"
             alt="logo"
-            width={200}
-            height={200}
+            width={100}
+            height={100}
             sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33vw"
           />
        </div>
