7a7d601d1cd3b65054cfcae2f887c1ea78a5ad66
AdrienW86 <adrien_weiss@outlook.fr>
2025-04-26 16:17:36 +0200

    mise en ligne

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -8,16 +8,9 @@ import { useInView } from 'react-intersection-observer';
 import styles from './banner.module.css';
 
 const images = [
-  '/image1.jpg',
-  '/image2.jpg',
-  '/image3.jpg', 
-  '/image4.jpg', 
-  '/image5.jpg',
-  '/image6.jpg',
-  '/image7.jpg',
-  '/image8.jpg',
-  '/image9.jpg',
-  '/image10.jpg',
+  '/image1.png',
+  '/image2.png',
+  '/image3.png', 
 ];
 
 export default function Carousel() {
--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -10,8 +10,8 @@ export default function Footer() {
     <footer className={styles.footer}>
        <div className={styles.container}>
         <Image
-            src="/logo2.png"
-            alt="Vue du château-hôtel"
+            src="/logo.png"
+            alt="logo"
             width={200}
             height={200}
             sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33vw"
